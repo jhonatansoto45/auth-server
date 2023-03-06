6f1d00eccf4641d666165a40a966099bf55dd388
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-03-06 14:20:46 -0500

    Manejo de rutas para la navegacion en angular

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const { dbConnection } = require("./database/config");
 require("dotenv").config();
 
@@ -20,6 +22,11 @@ app.use(express.json());
 
 //* RUTAS (MIDDLEWARE EXPERESS)
 app.use("/api/auth", require("./routes/auth"));
+
+//* MANEJO DE DEMAS RUTAS
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
