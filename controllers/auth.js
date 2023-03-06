960dba149c064fcb936e3e9e8dd64e7894b0ec31
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-03-06 12:10:13 -0500

    Respuesta de email

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,6 +35,7 @@ const crearUsuario = async (req, res = response) => {
       ok: true,
       uid: dbUser.id,
       name: name,
+      email: dbUser.email,
       token,
     });
   } catch (error) {
@@ -77,6 +78,7 @@ const loginUsuario = async (req, res = response) => {
       ok: true,
       uid: dbUser.id,
       name: dbUser.name,
+      email: dbUser.email,
       token,
     });
   } catch (error) {
@@ -90,14 +92,19 @@ const loginUsuario = async (req, res = response) => {
 };
 
 const revalidarToken = async (req, res = response) => {
-  const { uid, name } = req;
+  const { uid } = req;
 
-  const token = await generarJWT(uid, name);
+  //* LEER LA BASE DE DATOS
+  const dbUser = await Usuario.findById(uid);
+
+  //* GENERAR JWT
+  const token = await generarJWT(uid, dbUser.name);
 
   return res.json({
     ok: true,
     uid,
-    name,
+    name: dbUser.name,
+    email: dbUser.email,
     token,
   });
 };
